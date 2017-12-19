f94858449452b2664e23141938082a73ea2c575a
Pierre Marchand <pierre.jimthefrench@gmail.com>
2017-12-19 17:24:04 +0100

    wrapper htool updated

--- bemtool/miscellaneous/htool_wrap.hpp
+++ bemtool/miscellaneous/htool_wrap.hpp
@@ -7,18 +7,19 @@
 
 namespace bemtool{
 
-// template <typename KernelType, typename Discretization>
-// class BIO_Generator : public htool::IMatrix<Cplx>{
-//   BIOp<KernelType>& V;
-//   Dof<Discretization>& dof;
-//
-// public:
-//   BIO_Generator(BIOp<KernelType>& V0, Dof<Discretization>& dof0):IMatrix(NbDof(dof0),NbDof(dof0)), V(V0), dof(dof0) {}
-//
-//   Cplx get_coef(const int& i, const int& j) const {
-//     return V(dof.ToElt(i),dof.ToElt(j));
-//   }
-// };
+template <typename KernelType, typename Discretization>
+class BIO_Generator : public htool::IMatrix<Cplx>{
+  BIOp<KernelType>& V;
+  Dof<Discretization>& dof;
+
+public:
+  BIO_Generator(BIOp<KernelType>& V0, Dof<Discretization>& dof0):IMatrix(NbDof(dof0),NbDof(dof0)), V(V0), dof(dof0) {}
+
+  Cplx get_coef(const int& i, const int& j) const {
+      BIOp<KernelType> V_local=V;
+    return V_local(dof.ToElt(i),dof.ToElt(j));
+  }
+};
 
 template <typename KernelType, typename Discretization>
 class SubBIO_Generator : public htool::IMatrix<Cplx>{
